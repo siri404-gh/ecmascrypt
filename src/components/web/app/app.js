502a67fd5fb4ea1a83f81241e936983c71cde3f6
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-11-04 22:52:15 +0000

    updates

--- src/components/web/app/app.js
+++ src/components/web/app/app.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header title='firebase' />
+        {/* <Header title='Title' /> */}
         <Content />
         <Loader />
       </div>
