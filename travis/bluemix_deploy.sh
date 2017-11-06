9de1023ff8b0a5435dd4b4f8a70161d34434cd22
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-11-06 08:06:54 +0000

    fixes bluemix deploy

--- travis/bluemix_deploy.sh
+++ travis/bluemix_deploy.sh
@@ -1,4 +1,4 @@
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 
-cf login -u $BLUEMIX_USER -p $BLUEMIX_PASSWORD -a  https://api.eu-gb.bluemix.net -o $BLUEMIX_ORGANIZATION -s $BLUEMIX_SPACE
-cf push  $CLOUDFOUNDRY_APP -o $DOCKER_REPO
+./cf login -u $BLUEMIX_USER -p $BLUEMIX_PASSWORD -a  https://api.eu-gb.bluemix.net -o $BLUEMIX_ORGANIZATION -s $BLUEMIX_SPACE
+./cf push  $CLOUDFOUNDRY_APP -o $DOCKER_REPO
